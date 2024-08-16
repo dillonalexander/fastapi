ade6746577777bd8613fdd8000e09004718a8770
dillonwalexander <dillonwalexander@gmail.com>
2024-08-15 23:01:00 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+class ViewerCounter:
+    def __init__(self):
+        self.total_viewers = 0
+    def increment(self):
+        self.total_viewers += 1
+        return self.total_viewers
+    
+viewer_counter = ViewerCounter()
+
+@app.get("/")
+async def health_check():
+    return "The health check is successful!"
+
+
+@app.get("/hello")
+async def hello_creator():
+    viewers_count = viewer_counter.increment()
+    return f"Hey, you are viewer number {viewers_count}!"
+
+
